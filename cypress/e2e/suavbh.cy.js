69e30dd83a81b1fa0af027d4c5871baadeebc3a1
Suabh Bhattarai <suabh.bhattarai@itonics.de>
2022-07-15 12:34:05 +0545

    first commit

---
+++ cypress/e2e/suavbh.cy.js
@@ -0,0 +1,8 @@
+it('adds todos', () => {
+    cy.visit('https://todo.app.com')
+    cy.get('[data-testid="new-todo"]')
+      .type('write code{enter}')
+      .type('write tests{enter}')
+    // confirm the application is showing two items
+    cy.get('[data-testid="todos"]').should('have.length', 2)
+  })
